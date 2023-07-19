e7ea59aa98a5f0cfb245430924e810cbdda86cf8
Aaron aka Rooster <130356809+AaronRoss1589@users.noreply.github.com>
2023-07-19 12:25:32 -0700

    Create CalSync.js
    Starting JS page for calendar support.

---
+++ CalSync.js
@@ -0,0 +1,5 @@
+// this page is to be used to integrate calender support in the JavaScript environment.(the goal is, outlook, google, and CRM calendar support) 
+
+const addEvent(KronoSync):
+  Sync to a specified calendar;
+
