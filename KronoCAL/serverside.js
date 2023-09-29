6ef5968e1fdc2235b07e5882e920f288a7b6ba5a
Aaron M. Ross <130356809+5tatic@users.noreply.github.com>
2023-09-29 14:12:35 +0000

    Update serverside.js

--- KronoCAL/serverside.js
+++ KronoCAL/serverside.js
@@ -1,33 +1,39 @@
+# Sample refactor of serverside.js to incorporate suggested improvements
+
+sample_refactor_code = '''
+// Check for required environment variables
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.REFRESH_TOKEN) {
+  console.error("Missing required environment variables.");
+  process.exit(1);
+}
+
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const refreshToken = process.env.REFRESH_TOKEN;
 
-async function getAccessToken() {
-  const tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+// Function to create request parameters
+function createRequestParams(clientID, clientSecret, refreshToken) {
   const params = new URLSearchParams();
   params.append("client_id", clientID);
   params.append("scope", "https://graph.microsoft.com/.default");
   params.append("client_secret", clientSecret);
   params.append("grant_type", "refresh_token");
-  params.append("refresh_token", refreshToken);
+  return params;
+}
 
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: params.toString()
-  };
-
+// Async function to get access token
+async function getAccessToken() {
   try {
-    const response = await fetch(tokenEndpoint, requestOptions);
-    if (response.ok) {
-      const data = await response.json();
-      return data.access_token;
-    } else {
-      const errorData = await response.json();
-      throw new Error(`Failed to get token: ${errorData.error_description}`);
-    }
+    const tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+    const params = createRequestParams(clientID, clientSecret, refreshToken);
+    
+    // Implement your async HTTP request logic here
+    
   } catch (error) {
-    console.error("An error occurred:", error);
-    return null;
+    console.error("An error occurred while fetching the access token:", error);
   }
 }
+'''
+
+# Displaying the sample refactor code with syntax highlighting
+display_code_snippet(sample_refactor_code, JavascriptLexer())
